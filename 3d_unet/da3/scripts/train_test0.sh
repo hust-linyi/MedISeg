73b9032e338773b417a113e519a7969f2e364d71
hust-linyi <lin_yi@hust.edu.cn>
2022-06-20 17:00:17 +0800

    sm

--- 3d_unet/da3/scripts/train_test0.sh
+++ 3d_unet/da3/scripts/train_test0.sh
@@ -1,4 +1,4 @@
 cd /newdata/ianlin/CODE/seg_trick/3d_unet/da3
 
-python train.py --task da3 --fold 0 --train-gpus 0
-python test.py --task da3 --fold 0 --test-gpus 0 --test-test-epoch 0 
+python train.py --task da3 --fold 0
+python test.py --task da3 --fold 0 --test-test-epoch 0 
--- 3d_unet/patch64/scripts/train_test0.sh
+++ 3d_unet/patch64/scripts/train_test0.sh
@@ -1,4 +1,4 @@
 cd /newdata/ianlin/CODE/seg_trick/3d_unet/patch64
 
-python train.py --task patch64 --fold 0 --train-gpus 0
-python test.py --task patch64 --fold 0 --test-gpus 0 --test-test-epoch 0
+python train.py --task patch64 --fold 0 --train-gpus 0 --patch-size 64,64,64
+python test.py --task patch64 --fold 0 --test-gpus 0 --test-test-epoch 0 --patch-size 64,64,64
